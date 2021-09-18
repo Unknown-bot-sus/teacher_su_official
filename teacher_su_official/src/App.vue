1e938b93be7accec94688bdb123a1fdf4da0e218
Min Phone Myat <minphonemyat@Mins-MBP.lan>
2021-09-18 11:54:51 +0630

    bootstrap set up

--- teacher_su_official/src/App.vue
+++ teacher_su_official/src/App.vue
@@ -1,30 +1,28 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app">
+    <Navbar></Navbar>
+    <div class="container-fluid">
+      <router-view></router-view>
+    </div>
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Navbar from "./components/Navbar"
+  export default {
+    name:"App",
+    components :{Navbar}
   }
-}
+</script>
+
+<style lang="scss">
+    $primary:#103B7B;
+    $danger:#BD2222;
+    $secondary:#363636;
+    $light:#FFFFFF;
+    $dark:#000000;
+    @import "~bootstrap/scss/bootstrap";
+    .active{
+      background: #000;
+    }
 </style>
--- teacher_su_official/src/main.js
+++ teacher_su_official/src/main.js
@@ -3,4 +3,5 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+window.bootstrap = require("bootstrap")
 createApp(App).use(store).use(router).mount('#app')
