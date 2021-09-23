838fa671a773d6b5bc94f7750d5ca366ce395ea0
Unknown-bot-sus <albent2017@gmail.com>
2021-09-23 17:03:31 +0630

    small menu responsive now

---
+++ teacher_su_official_vue/src/components/Course.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="d-flex flex-column course-container mx-auto">
+        <h3 v-text="title" class="fw-bold"></h3>
+        <span v-text="description" class="description"></span>
+        <div class="line"></div>
+        <router-link :to="link" class="bg-transparent link">
+            Read More <i><i class="fad fa-chevron-right "></i></i>
+        </router-link>
+    </div>
+</template>
+<script>
+import Header from '../components/Header'
+export default {
+    name: 'Course',
+    components: {Header},
+    props: {
+        title: String,
+        description: String,
+        link: String,
+    }
+}
+</script>
+<style scoped>
+.course-container{
+    width: 20rem;
+    border: 1px solid var(--dark);
+    padding: 1rem;
+    margin-top: 1rem;
+} 
+
+.description{
+    display: inline-block;
+    margin: 1rem 0;
+}
+.line{
+    width: 90%;
+    height: 1px;
+    margin: 1rem 0;
+    background-color: var(--dark);
+}
+
+.link{
+    color: var(--lightgreen);
+    align-self: flex-end;
+}
+
+@media only screen and (max-width: 652px){
+    .course-container{
+        margin-top: 0;
+    }
+}
+</style>
--- teacher_su_official_vue/src/views/Career.vue
+++ teacher_su_official_vue/src/views/Career.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container-fluid">
+    <div class="container-fluid mx-auto">
          <div class="row">
             <div class="col-12 d-flex align-items-center px-3 px-md-5 py-2" style="background-color:#caccd1;">
                 <router-link :to="{name:'Home'}"> <i class="me-4 fs-4 fas fa-home text-dark"></i> </router-link>
