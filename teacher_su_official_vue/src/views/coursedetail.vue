f25fc909eb046fe8ec19aa661d4fbd5243cc68e1
Min Phone Myat <minphonemyat@Mins-MBP.lan>
2021-11-16 16:22:10 +0630

    ###

--- teacher_su_official_vue/src/views/coursedetail.vue
+++ teacher_su_official_vue/src/views/coursedetail.vue
@@ -1,48 +1,53 @@
 <template>
    <div class="container">
-       <div class="row">
-           <div class="col-12 d-flex align-items-center justify-content-start my-3 my-md-5">
+       <div class="row mt-4">
+           <h1 class="fs-1 fw-bolder text-center">{{ lists.name }}</h1>
+           <div class="col-12 col-lg-6 d-flex align-items-center justify-content-center my-3 my-md-5">
                <div>
                    <img :src="'http://api.teachersucenter.com' + lists.image" alt="" class="img-thumbnail" style="width:300px;">
                </div>
            </div>
-           <div class="col-12 my-3 my-md-5">
+           <div class="col-12 col-lg-6 my-3 my-md-5 d-flex align-items-center">
                <div>
-                   <h1 class="fs-1 fw-bolder">{{ lists.name }}</h1><p class="fs-5">{{ lists.content }}</p>
+                   <p class="fs-5" style="text-indent:3rem;">{{ lists.content }}</p>
                </div>
-           </div>
-           <div class="col-12 d-flex align-items-center justify-content-start my-3 my-md-5">
+           </div>        
+       </div>
+       <div class="row">
+           <h1 class="fs-1 fw-bolder text-center">Key facts</h1>
+           <div class="col-12 d-flex align-items-center justify-content-center my-3 my-md-5">
                <div>
-                   <h1 class="fs-1 fw-bolder">Key facts</h1>
+                   
                    <table>
                        <tbody>
                            <tr>
-                               <td>CEFR level:</td>
-                               <td>{{ lists.cefr_level }}</td>
+                               <td style="width:130px;">CEFR level:</td>
+                               <td style="width:190px;">{{ lists.cefr_level }}</td>
                            </tr>
                            <tr>
-                               <td>Scale score:</td>
-                               <td>{{ lists.scale_score }}</td>
+                               <td style="width:130px;">Scale score:</td>
+                               <td style="width:190px;">{{ lists.scale_score }}</td>
                            </tr>
                            <tr>
-                               <td>Test format:</td>
-                               <td>{{ lists.test_format }}</td>
+                               <td style="width:130px;">Test format:</td>
+                               <td style="width:190px;">{{ lists.test_format }}</td>
                            </tr>
                            <tr>
-                               <td>No. of papers:</td>
-                               <td>{{ lists.paper_count }}</td>
+                               <td style="width:130px;">No. of papers:</td>
+                               <td style="width:190px;">{{ lists.paper_count }}</td>
                            </tr>
                            <tr>
-                               <td>Exam length:</td>
-                               <td>{{ lists.test_length }}</td>
+                               <td style="width:130px;">Exam length:</td>
+                               <td style="width:190px;">{{ lists.test_length }}</td>
                            </tr>
                        </tbody>
                    </table>
                </div>
-           </div>          
+           </div>  
        </div>
+
        <div class="row">
-           <div class="my-5">
+           <div class="my-5 d-flex align-items-center justify-content-center">
                 <a href="https://www.facebook.com/messages/t/270139473932097" target="_blank" class="btn btn-outline-dark">Apply now</a> <router-link to="/register" class="text-dark mx-2">How to apply ?</router-link>
            </div>
            <p class="text-muted text-end">date posted : {{ lists.date_posted }}</p>
